fe8801a3ec221349e7bace8a63333722ae25dc7d
Cook1e <69702045+legendary-cookie@users.noreply.github.com>
2022-03-22 18:58:29 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rawUrl="https://raw.githubusercontent.com/legendary-cookie/base-configs/main"
+rawUrl="https://https://legendary-cookie.github.io/base-configs"
 curl -fsSL -o /config/velocity.toml "${rawUrl}/velocity.toml"
 mkdir -p /plugins/Kuvel
 curl -fsSL -o /plugins/Kuvel/config.yml "${rawUrl}/kuvel-conf.yml"
