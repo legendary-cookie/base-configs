529c748056c95a17322067279ce413e7c0233246
Cook1e <69702045+legendary-cookie@users.noreply.github.com>
2022-03-22 20:11:27 +0100

    some conf files

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 rawUrl="https://legendary-cookie.github.io/base-configs"
-curl -fsSL -o /config/velocity.toml "${rawUrl}/velocity.toml"
-mkdir -p /plugins/Kuvel
-curl -fsSL -o /plugins/Kuvel/config.yml "${rawUrl}/kuvel-conf.yml"
+if INIT_TYPE="proxy"; then
+    curl -fsSL -o /config/velocity.toml "${rawUrl}/velocity.toml"
+    mkdir -p /plugins/Kuvel
+    curl -fsSL -o /plugins/Kuvel/config.yml "${rawUrl}/kuvel-conf.yml"
+else
+    curl -fsSL -o /config/paper.yml "${rawUrl}/paper.yml"
+    curl -fsSL -o /config/spigot.yml "${rawUrl}/spigot.yml"
+fi
