fe2911bf01cb0bade87b7bdb446d514f012220bc
Cook1e <69702045+legendary-cookie@users.noreply.github.com>
2022-03-23 18:07:59 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rawUrl="https://legendary-cookie.github.io/base-configs"
-if INIT_TYPE="proxy"; then
+if [ "$INIT_TYPE" = "proxy" ]; then
     curl -fsSL -o /config/velocity.toml "${rawUrl}/velocity.toml"
     mkdir -p /plugins/Kuvel
     curl -fsSL -o /plugins/Kuvel/config.yml "${rawUrl}/kuvel-conf.yml"
